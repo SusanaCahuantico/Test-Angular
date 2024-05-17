360433427c1a1b150d0d03fec01dd10b84c93582
Susana Gisela Cahuantico Bobadilla <scahuantico@juntoz.com>
2024-05-17 01:04:12 -0500

    feat: task

--- my-angular-app/src/app/tasks/tasks.component.ts
+++ my-angular-app/src/app/tasks/tasks.component.ts
@@ -2,11 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tasks',
-  standalone: true,
-  imports: [],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent {
 
+  tasks: string[] = [];
+
+  addTask(): void {
+    const newTaskText = (document.getElementById("newTask") as HTMLInputElement)?.value ?? '';
+    if (newTaskText) { 
+      console.log('TASK')
+      this.tasks.push(newTaskText);
+    }
+  }
 }
